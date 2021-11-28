a99c8f61c91d655a9ed8369bcf051ea802b01f1f
Ayan <ayan@elsewhen.com>
2021-11-28 17:15:35 +0000

    add sendemail

---
+++ src/lambda/sendEmail.js
@@ -0,0 +1,9 @@
+export function handler(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Hello, World! ",
+      event: event,
+    }),
+  })
+}
