a8d1b487ce14d3406b950c027687246b2e2c28ba
Ayan <ayan@elsewhen.com>
2021-11-28 17:03:41 +0000

    remove repeat folder

--- src/lambda/hello.js
+++
@@ -1,9 +0,0 @@
-export function handler(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    // That's the status code which will be returned when the function is executed to indicates that everything is OK
-    body: JSON.stringify({
-      msg: "Hello, World! ",
-    }),
-  })
-}
