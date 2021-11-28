9c10b3cf4c1e0825f74cfdb1078d81204fc0fba1
Ayan <ayan@elsewhen.com>
2021-11-28 18:34:08 +0000

    update api and domain

--- src/lambda/sendEmail.js
+++ src/lambda/sendEmail.js
@@ -7,8 +7,8 @@ const formData = require("form-data")
 const Mailgun = require("mailgun.js")
 const mailgun = new Mailgun(formData)
 const mg = mailgun.client({
-  username: "api",
-  key: process.env.MAILGUN_API_KEY,
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
 })
 
 const headers = {
