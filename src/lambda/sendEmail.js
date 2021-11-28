ed9a54c000e2f6aaa88ddf2b57ca4d498deb7783
Ayan <ayan@elsewhen.com>
2021-11-28 19:12:20 +0000

    update sendEmail function

--- src/lambda/sendEmail.js
+++ src/lambda/sendEmail.js
@@ -11,10 +11,8 @@ const successCode = 200
 const errorCode = 400
 
 // connect to mailgun API
-const formData = require("form-data")
-const Mailgun = require("mailgun.js")
-const mailgun = new Mailgun(formData)
-const mg = mailgun.client({
+const mailgun = require("mailgun.js")
+const mg = mailgun({
   apiKey: process.env.MAILGUN_API_KEY,
   domain: process.env.MAILGUN_DOMAIN,
 })
